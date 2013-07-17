74038acfb749f8d4bde972a386aec1cf132bd1d6
Stefan Urbanek <stefan@agentfarms.net>
2013-07-17 00:13:52 -0500

    added window example

---
+++ examples/aggregate_over_window.py
@@ -0,0 +1,49 @@
+from bubbles import Pipeline, FieldList, data_object, open_store
+
+# Sample order data with fields:
+fields = FieldList(
+            ["id", "integer"],
+            ["customer_id", "integer"],
+            ["year", "integer"],
+            ["amount", "integer"]
+        )
+
+data = [
+    [1, 1, 2009, 10],
+    [2, 1, 2010, 20],
+    [3, 1, 2011, 20],
+    [4, 1, 2012, 50],
+    [5, 2, 2010, 50],
+    [6, 2, 2012, 40],
+    [7, 3, 2011, 100],
+    [8, 3, 2012, 150],
+    [9, 3, 2013, 120]
+]
+
+source = data_object("iterable", data, fields)
+
+# Stores for SQL alternative, if enabled (see below)
+stores = { "default": open_store("sql","sqlite:///") }
+
+#
+# Create the pipeline
+#
+
+p = Pipeline(stores=stores)
+p.source_object(source)
+
+# Uncomment this to get SQL operations instead of python iterator
+# p.create("default", "data")
+
+# Find last purchase date
+last_purchase = p.fork()
+last_purchase.aggregate(["customer_id"],
+                        [["year", "max"]],
+                        include_count=False)
+last_purchase.rename_fields({"year_max": "last_purchase_year"})
+p.join_details(last_purchase, "customer_id", "customer_id")
+
+p.pretty_print()
+
+p.run()
+
